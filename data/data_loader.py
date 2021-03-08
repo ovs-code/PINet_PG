414b18f12dde6a3cbd061c3b586dc12e9eb98aa4
Oskar von Seeler <oskar@vonseeler.de>
2021-03-08 17:11:47 +0100

    add missing visualizer, convert tabs to spaces

--- data/data_loader.py
+++ data/data_loader.py
@@ -21,30 +21,30 @@ def CreateDataset(opt):
     return dataset
 
 class CustomDatasetDataLoader():
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def name(self):
-		return 'CustomDatasetDataLoader'
+    def name(self):
+        return 'CustomDatasetDataLoader'
 
-	def initialize(self, opt):
-		self.opt = opt
-		self.dataset = CreateDataset(opt)
-		self.dataloader = torch.utils.data.DataLoader(
-			self.dataset,
-			batch_size=opt.batchSize,
-			shuffle=not opt.serial_batches,
-			num_workers=int(opt.nThreads))
+    def initialize(self, opt):
+        self.opt = opt
+        self.dataset = CreateDataset(opt)
+        self.dataloader = torch.utils.data.DataLoader(
+            self.dataset,
+            batch_size=opt.batchSize,
+            shuffle=not opt.serial_batches,
+            num_workers=int(opt.nThreads))
 
-	def load_data(self):
-		return self
+    def load_data(self):
+        return self
 
-	def __len__(self):
-		return min(len(self.dataset), self.opt.max_dataset_size)
+    def __len__(self):
+        return min(len(self.dataset), self.opt.max_dataset_size)
 
-	def __iter__(self):
-		for i, data in enumerate(self.dataloader):
-			if i >= self.opt.max_dataset_size:
-				break
-			yield data
+    def __iter__(self):
+        for i, data in enumerate(self.dataloader):
+            if i >= self.opt.max_dataset_size:
+                break
+            yield data
 
