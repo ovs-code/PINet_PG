fc34e2c6de40fc32d3cb32b9f93d1402fec5f17d
Oskar von Seeler <oskar.vonseeler@gmail.com>
2021-03-15 13:57:40 +0000

    add a script for inference

---
+++ scripts/use_star.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# DeepFashion
+
+# number of samples to evaluate
+how_many=3
+# set to 0 for GPU testing, -1 for CPU
+gpu_id=0
+
+python use.py --dataroot ./test_data/ --name fashion_PInet --model PInet --phase test --dataset_mode keypoint --norm instance --batchSize 1 --resize_or_crop no --gpu_ids $gpu_id --BP_input_nc 18 --no_flip --which_model_netG PInet --checkpoints_dir ./checkpoints --pairLst ./test_data/fasion-resize-pairs-test.csv --which_epoch latest --results_dir ./results --how_many $how_many
