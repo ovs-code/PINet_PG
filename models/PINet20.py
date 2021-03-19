436297e61343c32140c9360e75d35a18899e8008
Oskar von Seeler <oskar.vonseeler@gmail.com>
2021-03-19 11:58:47 +0100

    add individual optimizers for parsing and image

--- models/PINet20.py
+++ models/PINet20.py
@@ -110,11 +110,20 @@ class TransferModel(nn.Module):
             else:
                 raise Excption('Unsurportted type of L1!')
             # initialize optimizers
-            self.optimizer_G = torch.optim.Adam(self.netG.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+            opt_args = dict(lr=opt.lr, betas=(opt.beta1, 0.999))
+            self.optimizer_G = torch.optim.Adam(self.netG.parameters(), **opt_args)
+            # all parameters of self.netG except the parsing ones
+            image_params = itertools.chain(
+                self.netG.model.refine_attn.parameters(),
+                self.netG.model.app_trans_net.parameters(),
+                self.netG.model.refine_upsample_net.parameters()
+            )
+            self.optimizer_image_G = torch.optim.Adam(image_params, **opt_args)
+            self.optimizer_parsing_G = torch.optim.Adam(self.netG.model.ParsingNet.parameters(), **opt_args)
             if opt.with_D_PB:
-                self.optimizer_D_PB = torch.optim.Adam(self.netD_PB.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+                self.optimizer_D_PB = torch.optim.Adam(self.netD_PB.parameters(), **opt_args)
             if opt.with_D_PP:
-                self.optimizer_D_PP = torch.optim.Adam(self.netD_PP.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+                self.optimizer_D_PP = torch.optim.Adam(self.netD_PP.parameters(), **opt_args)
 
             self.optimizers = []
             self.schedulers = []
@@ -179,21 +188,46 @@ class TransferModel(nn.Module):
                   self.input_SPL1_onehot, self.input_SPL2_onehot]
         self.fake_p2, self.fake_parse = self.netG(G_input)
 
-    def forward_seperate(self):
+    # def forward_seperate(self):
+    #     self.input_P1 = Variable(self.input_P1_set)
+    #     self.input_P2 = Variable(self.input_P2_set)
+    #     self.input_KP1 = Variable(self.input_KP1_set)
+    #     self.input_KP2 = Variable(self.input_KP2_set)
+    #     self.input_SPL1 = Variable(self.input_SPL1_set)
+    #     self.input_SPL2 = Variable(self.input_SPL2_set)
+    #     self.input_SPL1_onehot = Variable(self.input_SPL1_onehot_set)
+    #     self.input_SPL2_onehot = Variable(self.input_SPL2_onehot_set)
+    #     parse_input = torch.cat((
+    #         self.input_P1,
+    #         self.input_SPL1_onehot,
+    #         torch.cat((self.input_KP1, self.input_KP2), 1)
+    #     ), 1)
+    #     self.fake_parse = self.netG.model.ParsingNet(parse_input)
+    #     G_input = [self.input_P1,
+    #                torch.cat((self.input_KP1, self.input_KP2), 1),
+    #               self.input_SPL1_onehot, self.input_SPL2_onehot]
+    #     self.fake_p2 = self.netG.model.forward_image(G_input)
+
+    def prepare_forward(self):
         self.input_P1 = Variable(self.input_P1_set)
-        self.input_P2 = Variable(self.input_P2_set)
         self.input_KP1 = Variable(self.input_KP1_set)
-        self.input_KP2 = Variable(self.input_KP2_set)
         self.input_SPL1 = Variable(self.input_SPL1_set)
-        self.input_SPL2 = Variable(self.input_SPL2_set)
+
+        self.input_P2 = Variable(self.input_P2_set)
+        self.input_KP2 = Variable(self.input_KP2_set)
+        self.input_SPL2 = Variable(self.input_SPL2_set) #bs 1 256 176
         self.input_SPL1_onehot = Variable(self.input_SPL1_onehot_set)
         self.input_SPL2_onehot = Variable(self.input_SPL2_onehot_set)
+
+    def forward_parsing(self):
         parse_input = torch.cat((
             self.input_P1,
             self.input_SPL1_onehot,
             torch.cat((self.input_KP1, self.input_KP2), 1)
         ), 1)
         self.fake_parse = self.netG.model.ParsingNet(parse_input)
+
+    def forward_image(self):
         G_input = [self.input_P1,
                    torch.cat((self.input_KP1, self.input_KP2), 1),
                   self.input_SPL1_onehot, self.input_SPL2_onehot]
@@ -271,9 +305,27 @@ class TransferModel(nn.Module):
         self.loss_mask =  self.loss_G_L1 + self.loss_G_GAN * self.opt.lambda_GAN+ self.maskloss1
         self.loss_mask.backward()
 
-    def backward_seperate_G(self):
+    # def backward_seperate_G(self):
+    #     mask = self.input_SPL2.squeeze(1).long()
+    #     self.maskloss1 = self.parseLoss(self.fake_parse, mask)
+    #     L1_per = self.criterionL1(self.fake_p2, self.input_P2)
+    #     self.loss_G_L1 = L1_per[0]
+    #     pred_fake = self.netD_PB(torch.cat((self.input_KP2, self.fake_p2),1))
+    #     pred_fake_pp = self.netD_PP(torch.cat((self.fake_p2,self.input_P1),1))
+
+    #     self.L1 = L1_per[1]
+    #     self.per = L1_per[2]
+    #     self.loss_G_GAN = (self.criterionGAN(pred_fake, True) + self.criterionGAN(pred_fake_pp, True))/2
+
+    #     self.loss_mask =  self.loss_G_L1 + self.loss_G_GAN * self.opt.lambda_GAN + self.maskloss1
+    #     self.loss_mask.backward()
+
+    def backward_parsing_G(self):
         mask = self.input_SPL2.squeeze(1).long()
         self.maskloss1 = self.parseLoss(self.fake_parse, mask)
+        self.maskloss1.backward()
+
+    def backward_image_G(self):
         L1_per = self.criterionL1(self.fake_p2, self.input_P2)
         self.loss_G_L1 = L1_per[0]
         pred_fake = self.netD_PB(torch.cat((self.input_KP2, self.fake_p2),1))
@@ -283,7 +335,7 @@ class TransferModel(nn.Module):
         self.per = L1_per[2]
         self.loss_G_GAN = (self.criterionGAN(pred_fake, True) + self.criterionGAN(pred_fake_pp, True))/2
 
-        self.loss_mask =  self.loss_G_L1 + self.loss_G_GAN * self.opt.lambda_GAN + self.maskloss1
+        self.loss_mask = self.loss_G_L1 + self.loss_G_GAN * self.opt.lambda_GAN
         self.loss_mask.backward()
 
     def optimize_parameters(self):
@@ -300,15 +352,22 @@ class TransferModel(nn.Module):
 
 
     def optimize_parameters_seperate(self):
-        self.forward_seperate()
+        self.prepare_forward()
+
+        self.forward_parsing()
+        self.optimizer_parsing_G.zero_grad()
+        self.backward_parsing_G()
+        self.optimizer_parsing_G.step()
+
+        self.forward_image()
         self.optimizer_D_PB.zero_grad()
         self.optimizer_D_PP.zero_grad()
         self.backward_D()
         self.optimizer_D_PB.step()
         self.optimizer_D_PP.step()
-        self.optimizer_G.zero_grad()
-        self.backward_seperate_G()
-        self.optimizer_G.step()
+        self.optimizer_image_G.zero_grad()
+        self.backward_image_G()
+        self.optimizer_image_G.step()
 
 
     def get_current_errors(self):
