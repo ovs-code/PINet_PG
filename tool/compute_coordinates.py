8644df8e37bfcd8f5201399b6039afa3f73829b8
Oskar von Seeler <oskar.vonseeler@gmail.com>
2021-03-24 15:26:38 +0100

    Implemented wrapper for HRNet model

--- tool/compute_coordinates.py
+++ tool/compute_coordinates.py
@@ -1,255 +1,96 @@
 import os
-import numpy as np
+from argparse import Namespace
+from typing import Tuple
 
-from keras.models import load_model
-import skimage.transform as st
-import pandas as pd
-from tqdm import tqdm
-from skimage.io import imread
-from skimage.transform import resize
-from scipy.ndimage import gaussian_filter
+import pkg_resources
+import torch
+import tqdm
+from hrnet_pose import models
+from hrnet_pose.config import cfg, update_config
+from hrnet_pose.core.inference import get_final_preds
+from PIL import Image
+from torchvision import transforms
 
-from .cmd_args import args
-
-
-mapIdx = [[31, 32], [39, 40], [33, 34], [35, 36], [41, 42], [43, 44], [19, 20], [21, 22],
-          [23, 24], [25, 26], [27, 28], [29, 30], [47, 48], [49, 50], [53, 54], [51, 52],
-          [55, 56], [37, 38], [45, 46]]
-
-limbSeq = [[2, 3], [2, 6], [3, 4], [4, 5], [6, 7], [7, 8], [2, 9], [9, 10],
-           [10, 11], [2, 12], [12, 13], [13, 14], [2, 1], [1, 15], [15, 17],
-           [1, 16], [16, 18], [3, 17], [6, 18]]
-threshold = 0.1
-boxsize = 368
-# scale_search = [0.5, 1, 1.5, 2]  # I dont know what this should do
-scale_search = [1]
+INPUT_SIZE = (384, 288)
+TRESHOLD = 0.05
+SCALE_FACTOR = 4
 
 
-def compute_cordinates(oriImg, heatmap_avg, paf_avg, th1=0.1, th2=0.05):
-    all_peaks = []
-    peak_counter = 0
+def pad_image(image: Image, target_size: Tuple[int, int], color='white') -> Image:
+    "Pad the input image to the left and bottom"
+    width, height = image.size
+    if width > target_size[0] or height > target_size[1]:
+        raise ValueError('Image too large in at least one dimension.')
 
-    for part in range(18):
-        map_ori = heatmap_avg[:, :, part]
-        map = gaussian_filter(map_ori, sigma=3)
-
-        map_left = np.zeros(map.shape)
-        map_left[1:, :] = map[:-1, :]
-        map_right = np.zeros(map.shape)
-        map_right[:-1, :] = map[1:, :]
-        map_up = np.zeros(map.shape)
-        map_up[:, 1:] = map[:, :-1]
-        map_down = np.zeros(map.shape)
-        map_down[:, :-1] = map[:, 1:]
-
-        peaks_binary = np.logical_and.reduce(
-            (map >= map_left, map >= map_right, map >= map_up, map >= map_down, map > th1))
-        peaks = list(zip(np.nonzero(peaks_binary)[1], np.nonzero(
-            peaks_binary)[0]))  # note reverse
-
-        peaks_with_score = [x + (map_ori[x[1], x[0]],) for x in peaks]
-        id = list(range(peak_counter, peak_counter + len(peaks)))
-        peaks_with_score_and_id = [
-            peaks_with_score[i] + (id[i],) for i in range(len(id))]
-
-        all_peaks.append(peaks_with_score_and_id)
-        peak_counter += len(peaks)
-
-    connection_all = []
-    special_k = []
-    mid_num = 10
-
-    for k in range(len(mapIdx)):
-        score_mid = paf_avg[:, :, [x-19 for x in mapIdx[k]]]
-        candA = all_peaks[limbSeq[k][0]-1]
-        candB = all_peaks[limbSeq[k][1]-1]
-        nA = len(candA)
-        nB = len(candB)
-        indexA, indexB = limbSeq[k]
-        if(nA != 0 and nB != 0):
-            connection_candidate = []
-            for i in range(nA):
-                for j in range(nB):
-                    vec = np.subtract(candB[j][:2], candA[i][:2])
-                    norm = np.sqrt(vec[0]*vec[0] + vec[1]*vec[1])
-                    vec = np.divide(vec, norm)
-
-                    startend = list(zip(np.linspace(candA[i][0], candB[j][0], num=mid_num),
-                                        np.linspace(candA[i][1], candB[j][1], num=mid_num)))
-
-                    vec_x = np.array([score_mid[int(round(startend[I][1])), int(round(startend[I][0])), 0]
-                                      for I in range(len(startend))])
-                    vec_y = np.array([score_mid[int(round(startend[I][1])), int(round(startend[I][0])), 1]
-                                      for I in range(len(startend))])
-
-                    score_midpts = np.multiply(
-                        vec_x, vec[0]) + np.multiply(vec_y, vec[1])
-                    score_with_dist_prior = sum(
-                        score_midpts)/len(score_midpts) + min(0.5*oriImg.shape[0]/norm-1, 0)
-                    criterion1 = len(np.nonzero(score_midpts > th2)[
-                                     0]) > 0.8 * len(score_midpts)
-                    criterion2 = score_with_dist_prior > 0
-                    if criterion1 and criterion2:
-                        connection_candidate.append(
-                            [i, j, score_with_dist_prior, score_with_dist_prior+candA[i][2]+candB[j][2]])
-
-            connection_candidate = sorted(
-                connection_candidate, key=lambda x: x[2], reverse=True)
-            connection = np.zeros((0, 5))
-            for c in range(len(connection_candidate)):
-                i, j, s = connection_candidate[c][0:3]
-                if(i not in connection[:, 3] and j not in connection[:, 4]):
-                    connection = np.vstack(
-                        [connection, [candA[i][3], candB[j][3], s, i, j]])
-                    if(len(connection) >= min(nA, nB)):
-                        break
-
-            connection_all.append(connection)
-        else:
-            special_k.append(k)
-            connection_all.append([])
-
-    # last number in each row is the total parts number of that person
-    # the second last number in each row is the score of the overall configuration
-    subset = -1 * np.ones((0, 20))
-    candidate = np.array([item for sublist in all_peaks for item in sublist])
-
-    for k in range(len(mapIdx)):
-        if k not in special_k:
-            partAs = connection_all[k][:, 0]
-            partBs = connection_all[k][:, 1]
-            indexA, indexB = np.array(limbSeq[k]) - 1
-
-            for i in range(len(connection_all[k])):  # = 1:size(temp,1)
-                found = 0
-                subset_idx = [-1, -1]
-                for j in range(len(subset)):  # 1:size(subset,1):
-                    if subset[j][indexA] == partAs[i] or subset[j][indexB] == partBs[i]:
-                        subset_idx[found] = j
-                        found += 1
-
-                if found == 1:
-                    j = subset_idx[0]
-                    if(subset[j][indexB] != partBs[i]):
-                        subset[j][indexB] = partBs[i]
-                        subset[j][-1] += 1
-                        subset[j][-2] += candidate[partBs[i].astype(
-                            int), 2] + connection_all[k][i][2]
-                elif found == 2:  # if found 2 and disjoint, merge them
-                    j1, j2 = subset_idx
-                    print("found = 2")
-                    # print("found = 2")
-                    membership = ((subset[j1] >= 0).astype(
-                        int) + (subset[j2] >= 0).astype(int))[:-2]
-                    if len(np.nonzero(membership == 2)[0]) == 0:  # merge
-                        subset[j1][:-2] += (subset[j2][:-2] + 1)
-                        subset[j1][-2:] += subset[j2][-2:]
-                        subset[j1][-2] += connection_all[k][i][2]
-                        subset = np.delete(subset, j2, 0)
-                    else:  # as like found == 1
-                        subset[j1][indexB] = partBs[i]
-                        subset[j1][-1] += 1
-                        subset[j1][-2] += candidate[partBs[i].astype(
-                            int), 2] + connection_all[k][i][2]
-
-                # if find no partA in the subset, create a new subset
-                elif not found and k < 17:
-                    row = -1 * np.ones(20)
-                    row[indexA] = partAs[i]
-                    row[indexB] = partBs[i]
-                    row[-1] = 2
-                    row[-2] = sum(candidate[connection_all[k][i,
-                                                              :2].astype(int), 2]) + connection_all[k][i][2]
-                    subset = np.vstack([subset, row])
-
-    # delete some rows of subset which has few parts occur
-    deleteIdx = []
-    for i in range(len(subset)):
-        if subset[i][-1] < 4 or subset[i][-2]/subset[i][-1] < 0.4:
-            deleteIdx.append(i)
-    subset = np.delete(subset, deleteIdx, axis=0)
-
-    if len(subset) == 0:
-        return np.array([[-1, -1]] * 18).astype(int)
-
-    cordinates = []
-    result_image_index = np.argmax(subset[:, -2])
-
-    for part in subset[result_image_index, :18]:
-        if part == -1:
-            cordinates.append([-1, -1])
-        else:
-            Y = candidate[part.astype(int), 0]
-            X = candidate[part.astype(int), 1]
-            cordinates.append([X, Y])
-    return np.array(cordinates).astype(int)
+    padded = Image.new('RGB', target_size, color)
+    padded.paste(image, (0, 0))
+    return padded
 
 
-def get_coords(oriImg, model):
-    """Use the pose estimating model to get a list of 18 keypoints for an input image."""
-    orig_shape = oriImg.shape
-    # manually resizing the images to get the script running
-    oriImg = st.resize(oriImg, (256, 256), preserve_range=True, order=1)
+class PoseEstimator:
 
-    multiplier = [x * boxsize / oriImg.shape[0] for x in scale_search]
+    def __init__(self, cfg, use_cuda):
+        self.model = models.pose_hrnet.get_pose_net(cfg, False)
+        if use_cuda:
+            self.model = self.model.cuda()
+        self.model.load_state_dict(torch.load(
+            cfg.TEST.MODEL_FILE), strict=False)
+        self.model.eval()
+        self.use_cuda = use_cuda
+        self.transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[
+                                 0.229, 0.224, 0.225])
+        ])
 
-    heatmap_avg = np.zeros((orig_shape[0], orig_shape[1], 19))
-    paf_avg = np.zeros((orig_shape[0], orig_shape[1], 38))
-
-    for m in range(len(multiplier)):
-        scale = multiplier[m]
-
-        new_size = (np.array(oriImg.shape[:2]) * scale).astype(np.int32)
-        imageToTest = resize(oriImg, new_size, order=3, preserve_range=True)
-        imageToTest_padded = imageToTest[np.newaxis, :, :, :]/255 - 0.5
-
-        output1, output2 = model(imageToTest_padded, training=False)
-
-        heatmap = st.resize(output2[0], orig_shape[:2],
-                            preserve_range=True, order=1)
-        paf = st.resize(output1[0], orig_shape[:2],
-                        preserve_range=True, order=1)
-        heatmap_avg += heatmap
-        paf_avg += paf
-
-    heatmap_avg /= len(multiplier)
-
-    pose_cords = compute_cordinates(oriImg, heatmap_avg, paf_avg)
-
-    return pose_cords
+    def infer(self, image: Image):
+        """Takes an input RGB image <= INPUT_SIZE and returns an (17 x 2)-array"""
+        width, height = image.size
+        if image.size != INPUT_SIZE:
+            image = pad_image(image, INPUT_SIZE)
+        with torch.no_grad():
+            batch = self.transform(image).unsqueeze(0)
+            if self.use_cuda:
+                batch = batch.cuda()
+            output = self.model(batch).detach().cpu().numpy()
+            preds, maxvals = get_final_preds(output)
+            points = preds * SCALE_FACTOR
+            invalid = (points[:, 0] >= width) | (
+                points[:, 1] >= height) | (maxvals[0, :, 0] < TRESHOLD)
+            points[invalid] = -1
+            return points
 
 
 if __name__ == '__main__':
     # most important parameters
     input_folder = './test_data/test/'
     output_path = './test_data/test-annotation-test.csv'
+    pose_estimator = './assets/pretrains/pose_hrnet_w48_384x288.pth'
 
-    args = args()
+    args = Namespace(
+        cfg=pkg_resources.resource_filename(
+            'hrnet_pose', 'yaml/coco/hrnet/w48_384x288_adam_lr1e-3.yaml'),
+        dataDir='.',
+        logDir='.',
+        modelDir='.',
+        opts=['TEST.MODEL_FILE', pose_estimator],
+        prevModelDir='.'
+    )
+    update_config(cfg, args)
 
-    model = load_model(args.pose_estimator)
+    model = PoseEstimator(cfg, use_cuda=True)
 
-    img_list = os.listdir(input_folder)
-
-    if os.path.exists(output_path):
-        processed_names = set(pd.read_csv(output_path, sep=':')['name'])
-        result_file = open(output_path, 'a')
-    else:
-        result_file = open(output_path, 'w')
+    with open(output_path, 'w') as result_file:
         processed_names = set()
         print('name:keypoints_y:keypoints_x', file=result_file)
 
-    # for image_name in tqdm(os.listdir(input_folder)):
-    for image_name in tqdm(img_list):
-        if image_name in processed_names:
-            continue
-        if '.ipynb_checkpoints' in image_name:
-            continue
+        for image_name in tqdm(os.listdir(input_folder)):
+            if '.ipynb_checkpoints' in image_name:
+                continue
 
-        oriImg = imread(os.path.join(input_folder, image_name))[
-            :, :, ::-1]  # B,G,R order
+            image = Image.open(os.path.join(input_folder, image_name))
+            pose_cords = model.infer(image)
 
-        pose_cords = get_coords(oriImg, model)
-
-        print("%s: %s: %s" % (image_name, str(list(pose_cords[:, 0])), str(
-            list(pose_cords[:, 1]))), file=result_file)
-        result_file.flush()
+            print("%s: %s: %s" % (image_name, list(pose_cords[:, 0]), list(
+                pose_cords[:, 1])), file=result_file)
+            result_file.flush()
